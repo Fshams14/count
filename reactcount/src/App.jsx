f9c7eb51008fc16830384ee46ab933c0cc0b608d
shams <farhanshams1410@gmail.com>
2026-05-16 14:13:27 +0530

    first commit

---
+++ reactcount/src/App.jsx
@@ -0,0 +1,11 @@
+import ReactCounter from './components/reactcounter';
+
+function App (){
+  return(
+    <div>
+      <ReactCounter />
+    </div>
+  )
+}
+
+export default App;
---
+++ reactcount/src/components/reactcounter.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import './reactcounter.css'
+
+
+const ReactCounter = () => {
+    const [count, setCount] = useState(0);
+  return (
+    <div className='counter-container'>
+        <p id='para'>You have clicked {count} times</p>
+        <button id='btn' onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  )
+}
+
+export default ReactCounter;
